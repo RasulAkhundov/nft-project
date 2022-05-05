150f89d3217e0c962606f32cbbb1f8ec21e97318
“Resulaxundov” <resul.axundov2002@mail.ru>
2022-05-05 17:12:27 +0400

    new changes

--- -/src/App.js
+++ -/src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
 import Collection from './components/Collection';
@@ -8,10 +9,23 @@ import Team from './components/Team';
 import Faq from './components/Faq';
 import Community from './components/Community';
 import Footer from './components/Footer';
+import $ from 'jquery';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  const pageLoaded = () => {
+    setLoading(true);
+    $("body").css("overflow-y", 'visible');
+  }
+
+  window.addEventListener('load', pageLoaded);
+
   return (
     <div className='app-container'>
+      <div className={loading ? 'preloader loader_deactive' : 'preloader'}>
+        <img src={require('../src/assets/preloader.png')} alt='preloader'/>
+      </div>
       <Header/>
       <Home/>
       <Collection/>
